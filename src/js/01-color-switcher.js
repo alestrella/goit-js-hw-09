b5e931ff838ed158d9f203a60da9f06435baf7ac
Alexandra Sabatin <s4batin@gmail.com>
2022-05-06 03:40:26 +0300

    add a Body Color Switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+// Write a script that, after clicking the "Start" button, changes the <body> background color once a second to a random value using the inline style. When clicking on the "Stop" button, background color change must stop.
 
+ref = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+}
+
+ref.btnStart.addEventListener('click', handleStartColorSwitcher);
+ref.btnStop.addEventListener('click', handleStopColorSwitcher)
+
+function handleStartColorSwitcher() {
+    ref.btnStart.setAttribute('disabled', 'disabled');
+    timertId = setInterval(changeBodyColor, 1000);
+}
+
+function handleStopColorSwitcher() {
+    ref.btnStart.removeAttribute('disabled', 'disabled');
+    clearInterval(timertId);
+}
+
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
